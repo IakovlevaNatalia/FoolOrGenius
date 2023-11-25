7f2dbda0428e53c7e6aa7163488b45a9e4affabe
Natalia Iakovleva <tourmalina@inbox.ru>
2023-11-25 12:53:06 -0500

    Add ability to add questions from user.

--- FoolOrGeniusConsoleApp/FileProvider.cs
+++ FoolOrGeniusConsoleApp/FileProvider.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace FoolOrGeniusConsoleApp
 {
     internal class FileProvider
     {
+        public static void Append(string fileName, string value)
+        {
+            var sw = new StreamWriter(fileName, true, Encoding.UTF8);
+            sw.WriteLine(value);
+            sw.Close();
+        }
+
+        public static string GetValue(string fileName)
+        {
+            var sr = new StreamReader(fileName, Encoding.UTF8);
+            var value = sr.ReadToEnd();
+            sr.Close();
+            return value;
+
+        }
+        public static bool Exists(string fileName)
+        {
+            return File.Exists(fileName);
+        }
     }
 }
--- FoolOrGeniusConsoleApp/QuestionsRepository.cs
+++ FoolOrGeniusConsoleApp/QuestionsRepository.cs
@@ -1,20 +1,48 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace FoolOrGeniusConsoleApp
 {
     class QuestionsRepository
     {
        public static  List<Question> GetAll()
-        {
-            var questions = new List<Question>();
+       {
+           var questions = new List<Question>();
 
-            questions.Add(new Question("Сколько будет два плюс два умноженное на два?", 6));
-            questions.Add(new Question("Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?", 9));
-            questions.Add(new Question("На двух руках 10 пальцев. Сколько пальцев на 5 руках?", 25));
-            questions.Add(new Question("Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?", 60));
-            questions.Add(new Question("Пять свечей горело, две потухли. Сколько свечей осталось?", 2));
+            if (FileProvider.Exists("questions.txt"))
+            {
+               var value = FileProvider.GetValue("questions.txt");
+               var lines = value.Split('\n');
+               foreach (var line in lines)
+               {
+                   var values = line.Split('#');
+                   var text = values[0];
+                   var answer = Convert.ToInt32(values[1]);
 
+                   var question = new Question(text, answer);
+
+                   questions.Add(question);
+               }
+            }
+            else
+            {
+                questions.Add(new Question("Сколько будет два плюс два умноженное на два?", 6));
+                questions.Add(new Question("Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?", 9));
+                questions.Add(new Question("На двух руках 10 пальцев. Сколько пальцев на 5 руках?", 25));
+                questions.Add(new Question("Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?", 60));
+                questions.Add(new Question("Пять свечей горело, две потухли. Сколько свечей осталось?", 2));
+
+                foreach (var question in questions)
+                {
+                    Add(question);
+                }
+            }
             return questions;
-        }
+       }
+       public static void Add(Question newQuestion)
+       {
+           var value = $"{newQuestion.Text}#{newQuestion.Answer}";
+           FileProvider.Append("questions.txt", value);
+       }
     }
 }
