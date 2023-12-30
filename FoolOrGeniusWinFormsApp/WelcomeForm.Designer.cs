2e8e66e1c594e054c8df86da8077982635bbd4ad
Natalia Iakovleva <tourmalina@inbox.ru>
2023-12-30 14:59:47 -0500

    Created authorizationForm. Created ability  to move form.

--- FoolOrGeniusWinFormsApp/WelcomeForm.Designer.cs
+++ FoolOrGeniusWinFormsApp/WelcomeForm.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.userNameTextBox = new System.Windows.Forms.TextBox();
             this.startButton = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
@@ -40,47 +38,47 @@
             this.questionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.addNewQuestionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.listOfAllQuestionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeButton = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.authorizationLabel = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.passwordFieldTextBox = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(207, 59);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(312, 32);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome To The Game";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(307, 113);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "What is your name?";
             // 
             // userNameTextBox
             // 
-            this.userNameTextBox.Location = new System.Drawing.Point(213, 141);
+            this.userNameTextBox.BackColor = System.Drawing.Color.White;
+            this.userNameTextBox.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userNameTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.userNameTextBox.Location = new System.Drawing.Point(235, 239);
             this.userNameTextBox.Name = "userNameTextBox";
-            this.userNameTextBox.Size = new System.Drawing.Size(292, 22);
+            this.userNameTextBox.Size = new System.Drawing.Size(303, 39);
             this.userNameTextBox.TabIndex = 2;
             this.userNameTextBox.TextChanged += new System.EventHandler(this.userNameTextBox_TextChanged);
             // 
             // startButton
             // 
-            this.startButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(15)))), ((int)(((byte)(255)))));
-            this.startButton.Location = new System.Drawing.Point(213, 197);
+            this.startButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(24)))), ((int)(((byte)(70)))));
+            this.startButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.startButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
+            this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Olive;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.ForeColor = System.Drawing.Color.White;
+            this.startButton.Location = new System.Drawing.Point(274, 463);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(292, 159);
+            this.startButton.Size = new System.Drawing.Size(222, 62);
             this.startButton.TabIndex = 3;
             this.startButton.Text = "Start";
             this.startButton.UseVisualStyleBackColor = false;
             this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            this.startButton.MouseEnter += new System.EventHandler(this.startButton_MouseEnter);
+            this.startButton.MouseLeave += new System.EventHandler(this.startButton_MouseLeave);
             // 
             // menuStrip1
             // 
@@ -90,7 +88,7 @@
             this.questionsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.Size = new System.Drawing.Size(726, 28);
             this.menuStrip1.TabIndex = 4;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -132,47 +130,126 @@
             this.addNewQuestionsToolStripMenuItem,
             this.listOfAllQuestionsToolStripMenuItem});
             this.questionsToolStripMenuItem.Name = "questionsToolStripMenuItem";
-            this.questionsToolStripMenuItem.Size = new System.Drawing.Size(88, 26);
+            this.questionsToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
             this.questionsToolStripMenuItem.Text = "Questions";
             // 
             // addNewQuestionsToolStripMenuItem
             // 
             this.addNewQuestionsToolStripMenuItem.Name = "addNewQuestionsToolStripMenuItem";
-            this.addNewQuestionsToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.addNewQuestionsToolStripMenuItem.Size = new System.Drawing.Size(219, 26);
             this.addNewQuestionsToolStripMenuItem.Text = "Add new questions";
             this.addNewQuestionsToolStripMenuItem.Click += new System.EventHandler(this.addNewQuestionsToolStripMenuItem_Click);
             // 
             // listOfAllQuestionsToolStripMenuItem
             // 
             this.listOfAllQuestionsToolStripMenuItem.Name = "listOfAllQuestionsToolStripMenuItem";
-            this.listOfAllQuestionsToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.listOfAllQuestionsToolStripMenuItem.Size = new System.Drawing.Size(219, 26);
             this.listOfAllQuestionsToolStripMenuItem.Text = "List of all questions";
             this.listOfAllQuestionsToolStripMenuItem.Click += new System.EventHandler(this.listOfAllQuestionsToolStripMenuItem_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.AutoSize = true;
+            this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.closeButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.closeButton.Location = new System.Drawing.Point(677, 13);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(37, 36);
+            this.closeButton.TabIndex = 5;
+            this.closeButton.Text = "X";
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            this.closeButton.MouseEnter += new System.EventHandler(this.closeButton_MouseEnter);
+            this.closeButton.MouseLeave += new System.EventHandler(this.closeButton_MouseLeave);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(24)))), ((int)(((byte)(70)))));
+            this.panel1.Controls.Add(this.authorizationLabel);
+            this.panel1.Controls.Add(this.closeButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(726, 163);
+            this.panel1.TabIndex = 7;
+            // 
+            // authorizationLabel
+            // 
+            this.authorizationLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.authorizationLabel.Font = new System.Drawing.Font("Times New Roman", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.authorizationLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.authorizationLabel.Location = new System.Drawing.Point(0, 0);
+            this.authorizationLabel.Name = "authorizationLabel";
+            this.authorizationLabel.Size = new System.Drawing.Size(667, 163);
+            this.authorizationLabel.TabIndex = 6;
+            this.authorizationLabel.Text = "Authorization";
+            this.authorizationLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.authorizationLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.authorizationLabel_MouseDown);
+            this.authorizationLabel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.authorizationLabel_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::FoolOrGeniusWinFormsApp.Properties.Resources._lock;
+            this.pictureBox2.Location = new System.Drawing.Point(143, 328);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 63);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FoolOrGeniusWinFormsApp.Properties.Resources.user;
+            this.pictureBox1.Location = new System.Drawing.Point(143, 239);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // passwordFieldTextBox
+            // 
+            this.passwordFieldTextBox.BackColor = System.Drawing.Color.White;
+            this.passwordFieldTextBox.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordFieldTextBox.Location = new System.Drawing.Point(235, 328);
+            this.passwordFieldTextBox.Name = "passwordFieldTextBox";
+            this.passwordFieldTextBox.Size = new System.Drawing.Size(303, 39);
+            this.passwordFieldTextBox.TabIndex = 9;
+            this.passwordFieldTextBox.UseSystemPasswordChar = true;
+            this.passwordFieldTextBox.TextChanged += new System.EventHandler(this.passwordFieldTextBox_TextChanged);
             // 
             // WelcomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(8)))), ((int)(((byte)(24)))));
+            this.ClientSize = new System.Drawing.Size(726, 598);
+            this.Controls.Add(this.passwordFieldTextBox);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.userNameTextBox);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "WelcomeForm";
             this.Text = "WelcomeForm";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.WelcomeForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.WelcomeForm_MouseMove);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         public System.Windows.Forms.TextBox userNameTextBox;
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.MenuStrip menuStrip1;
@@ -183,5 +260,11 @@
         private System.Windows.Forms.ToolStripMenuItem questionsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem addNewQuestionsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem listOfAllQuestionsToolStripMenuItem;
+        private System.Windows.Forms.Label closeButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label authorizationLabel;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox passwordFieldTextBox;
     }
 }
--- FoolOrGeniusWinFormsApp/WelcomeForm.cs
+++ FoolOrGeniusWinFormsApp/WelcomeForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace FoolOrGeniusWinFormsApp
@@ -8,6 +9,10 @@ namespace FoolOrGeniusWinFormsApp
         public WelcomeForm()
         {
             InitializeComponent();
+            this.passwordFieldTextBox.AutoSize = false;
+            this.passwordFieldTextBox.Size = new Size(300, 60);
+            this.userNameTextBox.AutoSize=false;
+            this.userNameTextBox.Size = new Size(300, 60);
         }
 
         private void userNameTextBox_TextChanged(object sender, EventArgs e)
@@ -18,6 +23,7 @@ namespace FoolOrGeniusWinFormsApp
         private void startButton_Click(object sender, EventArgs e)
         {
             Close();
+
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e) // exit from menuStrip
@@ -56,6 +62,71 @@ namespace FoolOrGeniusWinFormsApp
         {
             var questionListForm = new QuestionsListForm();
             questionListForm.ShowDialog();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e) 
+        {
+            
+            Application.Exit();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void passwordFieldTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_MouseEnter(object sender, EventArgs e)
+        {
+            startButton.ForeColor = Color.Green;
+        }
+
+        private void startButton_MouseLeave(object sender, EventArgs e)
+        {
+            startButton.ForeColor= Color.White;
+        }
+
+        private void closeButton_MouseEnter(object sender, EventArgs e)
+        {
+            closeButton.ForeColor= Color.Red;
+        }
+
+        private void closeButton_MouseLeave(object sender, EventArgs e)
+        {
+            closeButton.ForeColor=Color.White;
+        }
+
+        private Point lastPoint;
+        private void WelcomeForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top+=e.Y- lastPoint.Y;
+            }
+        }
+
+        private void WelcomeForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void authorizationLabel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left+=e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y; 
+            }
+        }
+
+        private void authorizationLabel_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
         }
     }
 }
