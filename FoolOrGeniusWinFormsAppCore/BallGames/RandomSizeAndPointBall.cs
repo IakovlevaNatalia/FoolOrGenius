0be752e5a1625a1e170b807b8c889eefc2ba683b
Natalia <balakov@myyahoo.com>
2024-02-01 16:09:10 -0500

    Started Animation

---
+++ FoolOrGeniusWinFormsAppCore/BallGames/RandomSizeAndPointBall.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace FoolOrGeniusWinFormsApp.BallGames
+{
+    public class RandomSizeAndPointBall:RandomPointBall
+
+    {
+        public RandomSizeAndPointBall(MainFormBallGameWinFormsApp mainFormBallGameWinFormsApp):base(mainFormBallGameWinFormsApp)
+        {
+            size = random.Next(30, 80);
+        }
+    }
+}
